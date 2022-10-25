c43f2e2d7fe15239a3a0d940100bec76195f706d
jo-1842 <102668525+jo-1842@users.noreply.github.com>
2022-10-25 11:55:12 +0200

    Add leading team display

--- index.js
+++ index.js
@@ -19,4 +19,28 @@ function resetScore() {
     homeScore = 0;
     document.getElementById("home-score").textContent = homeScore;
     document.getElementById("guest-score").textContent = guestScore;
+    document.getElementById("home-leading").textContent = "";
+    document.getElementById("guest-leading").textContent = "";
+
 }
+
+
+let homeLead 
+let guestLead 
+
+
+function indicateLeader() {
+    if (guestScore > homeScore){
+        guestLead = "\\o/";
+        homeLead = "";
+    } else if (guestScore < homeScore){
+        guestLead = "" ;
+        homeLead = "\\o/";
+    } else {
+        guestLead = "" ;
+        homeLead = "" ;
+    }
+    document.getElementById("home-leading").textContent = homeLead;
+    document.getElementById("guest-leading").textContent = guestLead;
+}
+
